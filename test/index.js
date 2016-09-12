79e1204c12a1891325ec8b0cdc516a06d76e37d8
Arron Mabrey <arron@mabreys.com>
2016-09-12 17:11:19 -0400

    refactor tests

--- test/index.js
+++ test/index.js
@@ -1,22 +1,15 @@
+var expect = require("expect");
 var loop = require("../index.js").default;
 
-var inc = loop(function(recur, x, n) {
-    if (x == n) {
-        return x;
-    } else {
-        return recur(x + 1, n);
-    }
-});
 
-try {
-    if (inc(0, 1000000) == 1000000) {
-        console.log("PASS");
-        process.exit(0);
-    } else {
-        console.log("FAIL");
-        process.exit(1);
-    }
-} catch(e) {
-    console.log("BOOM - " + e.message);
-    process.exit(1);
-}
+(function test () {
+    var fn = loop(function(recur, x, n) {
+        if (x == n) {
+            return x;
+        } else {
+            return recur(x + 1, n);
+        }
+    });
+
+    expect(fn(0, 1000000)).toEqual(1000000);
+})();
